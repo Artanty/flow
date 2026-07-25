55cf3c74127dccc4c3ee53b9f51e4b86989531ee
Artanty <thomasway7@gmail.com>
2026-07-25 11:58:29 +0300

    trig deploy ;

--- back/app.js
+++ back/app.js
@@ -90,6 +90,7 @@ async function triggerWorkflow(namespace, repo_name, commit_message, pat, safe_u
 }
 
 app.post('/webhook', async (req, res) => {
+  console.log(res.body)
   const repo_name = req.body.repository.name;
   const payload = JSON.stringify(req.body);
   const signature = req.headers['x-hub-signature-256'];
